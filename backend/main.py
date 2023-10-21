ad91788b371797b0fc16919d31e8fbd30c4d8e58
jaolson88 <olsonja88@gmail.com>
2023-10-21 14:08:13 -0500

    Feat: Create a set of data to retrieved at '/' from api.  Handle and log errors.  Move .gitignore into backend directory, make sure .venv dir is ignored.

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
-import logging
-from typing import Union
+import logging, uuid, datetime
+from dateutil.relativedelta import relativedelta
 from fastapi import FastAPI, HTTPException
 from starlette.responses import JSONResponse
 
@@ -8,8 +8,15 @@ app = FastAPI()
 logging.basicConfig(level=logging.INFO)
 
 @app.get("/")
-def read_root():
-    return {"Hello" : "World"}
+def get_data():
+    unique_uuid = str(uuid.uuid4())
+    current_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    expires_at = (current_time + relativedelta(months=1)).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    return {"uuid" : unique_uuid,
+            "document_name": "Super Top Secret Halloween Costume Ideas",
+            "current_time": current_time,
+            "description": "A list of the best halloween costumes.",
+            "expires_at": expires_at}
 
 @app.exception_handler(HTTPException)
 async def custom_http_exception_handler(request, exc):
